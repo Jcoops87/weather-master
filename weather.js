5011dc7dc8323017402119c456ba335c6a993a2c
Jcoops87 <jennycooper@jennycooper-macbookair3.roam.corp.google.com>
2016-02-03 14:41:55 -0600

    final

--- weather.js
+++ weather.js
@@ -2,12 +2,21 @@ $(function() {
   var handleWeatherResponse = function(data) {
     console.log(data);
 
-    // Put your code here to change the "markup" variable. 
+    // Put your code here to change the "markup" variable.
     // Don't change any other code in this file. You will be sad.
-    
-    var markup = "The weather report... " + "will be here when I finish my homework.";
-    
-    
+
+    var markup = "<h1>Today's weather forecast </h1>" +
+    "<ul> <li> Temperature: " + data.currently.apparentTemperature + " degrees </li>" +
+    "<li> Chance of Rain: " + data.currently.precipProbability + "% </li>" +
+    "<li> Humidity: " + data.currently.humidity + "% </li> </ul>" +
+
+    "<h2>Three Day Forecast </h2>" +
+    "<ul> <li> Tomorrow: " + data.daily.data[1].apparentTemperatureMin + " degrees min. " + data.daily.data[1].apparentTemperatureMax + " degrees max </li>" +
+    "<li> The day after tomorrow: " + data.daily.data[2].apparentTemperatureMin + " degrees min. " + data.daily.data[2].apparentTemperatureMax + " degrees max </li>" +
+    "<li> The day after, the day after tomorrow : " + data.daily.data[3].apparentTemperatureMin + " degrees min. " + data.daily.data[3].apparentTemperatureMax + " degrees max </li>" ;
+
+
+
     // End of your code
 
     $('.weather-report').html(markup);
